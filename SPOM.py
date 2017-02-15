6d58737fec19179e048b5370b3fe6c2a5bbbb0c0
jrhovet <jrhovet@gmail.com>
2017-02-15 09:49:58 -0500

    changed reduction factor to multiplication factor and changed > < problem

--- SPOM.py
+++ SPOM.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 DISTRIBUTION_MATRIX = "./JH_WS_DistributionMatrixCSV.csv"
 PROB_EXTINCTION_VECTOR = "./JH_WS_ExtinctionVectorCSV.csv"
 
-NUM_TRIALS = 1000
+NUM_TRIALS = 10000
 
 DISTANCE_UNIT_FACTOR = 75
 
-PROB_EXTINCTION_REDUCTION_FACTOR   = 1
-PROB_COLONIZATION_REDUCTION_FACTOR = 1
+PROB_EXTINCTION_MULTIPLICATION_FACTOR   = 1
+PROB_COLONIZATION_MULTIPLICATION_FACTOR = 1
 
 ### READ DATA INTO FORMAT WE WANT
 # pandas (pd.anything) lets us read csv's really easily, we then convert them into numpy arrays, which allows us to do linear algebra really quickly and easily
@@ -21,10 +21,10 @@ Pe_db = pd.read_csv(PROB_EXTINCTION_VECTOR)
 
 dist_db = dist_db.fillna(1e5) #replace NaN's with big number so that the log function turns them to 0 probability
 
-Pe = (np.array(Pe_db.Pe)[:,1]).astype("float")
+Pe = (np.array(Pe_db.Pe)[:]).astype("float")
 dist = (np.array(dist_db)[:,1:]).astype("float")
 
-Pe = Pe / PROB_EXTINCTION_REDUCTION_FACTOR #in case we want to mess with the numbers a bit, we can divide the extinction vector by a scalar
+Pe = Pe * PROB_EXTINCTION_MULTIPLICATION_FACTOR #in case we want to mess with the numbers a bit, we can divide the extinction vector by a scalar
 
 ### Transform distance matrix into probability of colonization matrix
 #parameters found by regression to fit given ln curve: y ~ a * ln(x) + b
@@ -43,12 +43,12 @@ for i in range(NUM_TRIALS): #Run the test a certain number of times
     random2 = np.random.rand(dist.shape[0],dist.shape[1])
 
     isAlive = np.zeros(Pe.shape)
-    isAlive[np.where(Pe > random1)] = 1 #roll dice, see if patch is still alive
+    isAlive[np.where(Pe < random1)] = 1 #roll dice, see if patch is still alive
 
 
     Pc = dist * isAlive #elementwise multiply to eliminate colonization from dead cells, still allows colonization TO dead cells
 
-    Pc = Pc/ PROB_COLONIZATION_REDUCTION_FACTOR #mess with numbers
+    Pc = Pc * PROB_COLONIZATION_MULTIPLICATION_FACTOR #mess with numbers
 
     PcMirrored = Pc + np.transpose(Pc) #add colonization to and colonization from (normal and transposed matricies)
 
